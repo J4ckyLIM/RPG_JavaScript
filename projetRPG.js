9e67ff8a7aba20f7698fd147188bb9851ef268fb
Jacky LIM <jacky.lim@edu.itescia.fr>
2018-12-05 19:43:23 +0100

    Rajout des potions STR/AGI/HEALING + Creation des monstres

--- projetRPG.js
+++ projetRPG.js
@@ -4,27 +4,70 @@ var hero = {
     agility: 10,
     health: 100,
     name: "",
-    inventory: "",
+    inventory : [],
     gold: "",
 };
 
+// Bouton play (projetRPG.html) 
 document.getElementById('start').addEventListener('click', play);
 
 function play(){
     console.log(hero);
 }
 
-// Potion de soin
-function healthPot(){
+// Potion de soin + bouton 
+document.getElementById('healthPot').addEventListener('click', healingPot);
+
+function healingPot(){
     for(let hp of Object.keys(hero.health)){
         parseInt(hp)+20;
         console.log();
     }
 }
-// Potion de force
+// Potion de force + bouton
+document.getElementById('strPot').addEventListener('click', strengthPot);
 
-// Potion d'endurance
-// Potion d'agilité
+function healingPot(){
+    for(let str of Object.keys(hero.strength)){
+        parseInt(str)+20;
+        console.log();
+    }
+}
+
+// Potion d'endurance + bouton
+/*document.getElementById('healthPot').addEventListener('click', )
+function healingPot(){
+    for(let hp of Object.keys(hero.health)){
+        parseInt(hp)+20;
+        console.log();
+    }
+}
+*/
+
+// Potion d'agilité + bouton
+document.getElementById('agiPot').addEventListener('click', agilityPot);
+
+function agilityPot(){
+    for(let agi of Object.keys(hero.agility)){
+        parseInt(agi)+20;
+        console.log();
+    }
+}
 // Caractéristique du monstre 1
+var macron = {
+    strength: 5,
+    stamina: 7,
+    health: stamina,
+}
 // Caractéristique du monstre 2
-// Caractéristique du monstre 
+var gilletJaune = {
+    strength: 5,
+    stamina: 7,
+    health: stamina,
+}
+// Caractéristique du monstre 3
+var trump = {
+    strength: 5,
+    stamina: 7,
+    health: stamina,
+}
