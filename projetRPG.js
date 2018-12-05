92b4293f8276e3bd1a275178e923a2ba003d7051
Unknown <jacky.lim@edu.itescia.fr>
2018-12-05 17:08:31 +0100

    Rajout de potion HP

--- projetRPG.js
+++ projetRPG.js
@@ -1,21 +1,28 @@
 // Caractéristique du personnage
-var hero ={
+var hero = {
     strength: 10,
     agility: 10,
     health: 100,
     name: "",
     inventory: "",
     gold: "",
-}
+};
 
-document.getElementById('start').addEventListener('click', play)
+document.getElementById('start').addEventListener('click', play);
 
 function play(){
     console.log(hero);
 }
 
 // Potion de soin
+function healthPot(){
+    for(let hp of Object.keys(hero.health)){
+        parseInt(hp)+20;
+        console.log();
+    }
+}
 // Potion de force
+
 // Potion d'endurance
 // Potion d'agilité
 // Caractéristique du monstre 1
