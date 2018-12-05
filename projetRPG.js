848c121c22bd4b95754d441bbf2a81201e48967b
Unknown <jacky.lim@edu.itescia.fr>
2018-12-05 16:41:18 +0100

    Création du bouton de jeu

--- projetRPG.js
+++ projetRPG.js
@@ -1,8 +1,23 @@
+// Caractéristique du personnage
 var hero ={
     strength: 10,
     agility: 10,
     health: 100,
     name: "",
     inventory: "",
-    
+    gold: "",
 }
+
+document.getElementById('start').addEventListener('click', play)
+
+function play(){
+    console.log(hero);
+}
+
+// Potion de soin
+// Potion de force
+// Potion d'endurance
+// Potion d'agilité
+// Caractéristique du monstre 1
+// Caractéristique du monstre 2
+// Caractéristique du monstre 
