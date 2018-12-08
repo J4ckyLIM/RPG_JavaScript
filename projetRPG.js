cbfebd7f5e3fe9d3cf83e81aa2375a6d4c8b3a8b
Jacky LIM <jacky.lim@edu.itescia.fr>
2018-12-08 16:45:02 +0100

    Ajout du bouton d'accès à l'inventaire

--- projetRPG.js
+++ projetRPG.js
@@ -104,7 +104,14 @@ function randomMonster(){
 
 // Fonction d'affichage du shop
 
+function linkShopButton(){
+    document.getElementById('shop').addEventListener('click',displayShop);
+}
+
+
 function displayShop(){
+    document.getElementById('commands').innerHTML = '';
+    document.getElementById('commands').innerHTML = '<button id="inventory">Acceder à l\'inventaire</button>';
     document.getElementById('fightLog').innerHTML = '';
     document.getElementById('fightLog').innerHTML = '<div id="shopDisplay"></div>';
     document.getElementById('shopDisplay').innerHTML =  '<i class="pots" id ="healingPot"></i>'; //LIER LES ID AUX IMAGES DE POTIONS
@@ -117,18 +124,24 @@ function displayShop(){
     document.getElementById('staminaPot').addEventListener('click',buyingFunction);
 }
 
+// Fonction d'affichage de l'inventaire
+
+function displayInventory(){
+    document.getElementById('inventory').innerHTML = 
+}
+
 // Bouton d'accès au shop
 
 function shopAccess(){  
     document.getElementById('commands').innerHTML = '<button id="shop">Acceder à la boutique</button>';
+    linkShopButton();
 }
 
-// Creation de la fonction d'attaque du héros + click bouton
-function linkButton(){
+// fonction d'attaque du héros + click bouton
+function linkAttackButton(){
     document.getElementById('attack').addEventListener('click', heroAttack);
-    document.getElementById('shop').addEventListener('click',displayShop);
 }
-linkButton();
+linkAttackButton();
 function heroAttack(){
     document.getElementById('divAttack').innerHTML = '<button id="attack">Attaquer</button>';
     monster.stamina.health -= hero.strength;
@@ -153,7 +166,7 @@ function heroAttack(){
     console.log(monster.stamina.health);
 }
  
-// Creation de la fonction d'attaque du monstre 
+// fonction d'attaque du monstre 
 
 function monsterAttack(){
     hero.stamina.health -= monster.strength;
@@ -161,7 +174,7 @@ function monsterAttack(){
         displayHeroHP();
         document.getElementById('msgFight').innerHTML = 'Vous avez subis ' + monster.strength + ' dégats';
         document.getElementById('divAttack').innerHTML = '<button id="attack">Attaquer</button>';
-        linkButton();
+        linkAttackButton();
     }
     else{
         displayHeroHP();
